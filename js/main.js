474f0d9381850332fc48d84f39a3cc8bac6b273c
belabelaisabela <isabelagualberto7@gmail.com>
2023-08-21 10:55:11 -0400

    Commit final

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ function calculo() {
   const altura = document.getElementById("altura").value;
 
   const imc = peso / (altura * altura);
-
   nomeresult.textContent = nome;
   idaderesult.textContent = idade + " anos";
   sexoresult.textContent = sexo;
@@ -21,24 +20,27 @@ function calculo() {
   pesoresult.textContent = peso + "kg";
   results.textContent = Math.floor(imc);
 
-  if (imc < 18.5) {
-    listado.style.color = "#FF3636";
-    listado.textContent = "Abaixo do peso";
-  } else if ((imc > 18.5) & (imc < 24.9)) {
-    listado.style.color = "#47AA25";
+  if (imc < 18.5) { 
+    list.style.color = "#FF2400";
+    list.textContent = "Abaixo do peso"; }
 
-    listado.textContent = "Peso normal";
-  } else if ((imc > 25.0) & (imc < 29.9)) {
-    listado.style.color = "#efff11";
-    listado.textContent = "Excesso de peso";
-  } else if ((imc > 30.0) & (imc < 34.9)) {
-    listado.style.color = "#FF3636";
-    listado.textContent = "Obsidade classe I";
-  } else if ((imc > 35.0) & (imc < 39.9)) {
-    listado.style.color = "#FF3636";
-    listado.textContent = "Obsidade classe II";
-  } else if (imc > 40) {
-    listado.style.color = "#FF3636";
-    listado.textContent = "Obsidade classe III";
-  }
+    else if ((imc > 18.5) & (imc < 24.9)) { 
+    list.style.color = "#3CB043";
+    list.textContent = "Peso normal"; } 
+
+    else if ((imc > 25.0) & (imc < 29.9)) {
+    list.style.color = "#FF2400";
+    list.textContent = "Excesso de peso"; }
+
+    else if ((imc > 30.0) & (imc < 34.9)) {
+    list.style.color = "D30000";
+    list.textContent = "Obesidade classe I"; }
+
+    else if ((imc > 35.0) & (imc < 39.9)) {
+    list.style.color = "D30000";
+    list.textContent = "Obesidade classe II"; }
+
+    else if (imc > 40) {
+    list.style.color = "#D30000";
+    list.textContent = "Obesidade classe III"; }
 }
